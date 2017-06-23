0159c9f79fcc3c74cec8c7252cccaff058a5f7fc
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-06-22 23:46:34 -0300

    Runner to implement the whole code

--- src/strategy/Runner.kt
+++ src/strategy/Runner.kt
@@ -1,9 +1,24 @@
 package strategy
 
+import strategy.languages.EnglishLanguage
+import strategy.languages.GermanLanguage
+import strategy.languages.PortugueseLanguage
+import strategy.occupations.DoctorOccupation
+import strategy.occupations.SoftwareDeveloperOccupation
+import strategy.occupations.TeacherOccupation
+
 /**
  * Created by daigomatsuoka on 22/06/17.
  */
 
 fun main(args: Array<String>) {
 
+    val marceloPerson = Person("Marcelo", PortugueseLanguage(), SoftwareDeveloperOccupation())
+    marceloPerson.introduce()
+
+    val kellyPerson = Person("Kelly", EnglishLanguage(), TeacherOccupation())
+    kellyPerson.introduce()
+
+    val fridaPerson = Person("Frida", GermanLanguage(), DoctorOccupation())
+    fridaPerson.introduce()
 }
