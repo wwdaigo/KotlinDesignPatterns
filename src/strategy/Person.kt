e97a28d2cc9538e47f634b508b70a6b5e3cbbc26
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-06-22 23:41:33 -0300

    Create the Person class

--- src/strategy/Person.kt
+++ src/strategy/Person.kt
@@ -1,8 +1,23 @@
 package strategy
 
+import strategy.interfaces.ILanguage
+import strategy.interfaces.IOccupation
+
 /**
  * Created by daigomatsuoka on 22/06/17.
  */
 
-class Person {
+class Person (
+        val name: String,
+        val language: ILanguage,
+        val occupation: IOccupation) {
+
+    fun introduce() {
+        language.sayHi()
+        println("My name is ${name}.")
+        println("My native language is ${language.name}, as we officialy call ${language.officialName}")
+        println("I work as ${occupation.name.toLowerCase()}.")
+        occupation.work()
+        language.sayThankYou()
+    }
 }
