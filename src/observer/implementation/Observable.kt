e0aa84475bdb1f730fc1aa0961c902156793647c
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-06-23 23:58:46 -0300

    Implement observable class

---
+++ src/observer/implementation/Observable.kt
@@ -0,0 +1,27 @@
+package observer.implementation
+
+import observer.interfaces.IObservable
+import observer.interfaces.IObserver
+
+/**
+ * Created by daigomatsuoka on 23/06/17.
+ */
+
+class Observable: IObservable {
+
+    val observers = ArrayList<IObserver>()
+
+    override fun add(o: IObserver) {
+        observers.add(o)
+    }
+
+    override fun remove(o: IObserver) {
+        observers.remove(o)
+    }
+
+    override fun notifyObservers() {
+        for (o in observers) {
+            o.update()
+        }
+    }
+}
--- src/observer/interfaces/IObserver.kt
+++ src/observer/interfaces/IObserver.kt
@@ -4,4 +4,6 @@ package observer.interfaces
  * Created by daigomatsuoka on 23/06/17.
  */
 interface IObserver {
+
+    fun update()
 }
